5a5dd400c29369c04ceb4d62fd716a9792f1c36d
Boffiro <71046033+Boffiro@users.noreply.github.com>
2022-09-06 09:58:29 +0200

    Init
    Initialisation projet

---
+++ FonctionsCustomPerso.R
@@ -0,0 +1,16 @@
+
+### Ensemble de fonctions custom pouvant être utiles grapillées 
+
+# Fonction pour détacher tout les packages chargés
+
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+  
+  package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+  
+  package.list <- setdiff(package.list,basic.packages)
+  
+  if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+     
+}
+
